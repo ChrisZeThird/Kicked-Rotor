19293dea2cf4d13f82b2adc3f417a877dbb8920b
Chris Ze Third <86256324+ChrisZeThird@users.noreply.github.com>
2022-06-03 14:11:03 +0200

    Update Orthogonal.py
    Update of the method to gather the level spacing distributions

--- Quantum/LevelSpacing/Orthogonal.py
+++ Quantum/LevelSpacing/Orthogonal.py
@@ -8,47 +8,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import KickedRotor1 as kr
 
-## Parameters
-
-nbeta = 500 
-m      = 7
-N      = 2**m
-K      = 10
-t      = 10
-kb     = 1
-
-p = np.linspace(start=-N, stop=N, endpoint=False, num=2*N) # impulsions
-x = np.pi/N * p                                    # positions
-
-f    = np.zeros(2*N) # setting up initial distribution
-f[N] = 1             # Dirac initial distribution in p = 0
-
-## Numerical application
-
-sim = kr.KickedRotor(kb,True)
-
-# psi_final1    = sim.avgPsi(x, p, f, K, t, nbeta)
-# plt.figure(1)
-# plt.plot(p, psi_final1, label=f'K={K}, tkick={t}, kb={kb}, nbeta={nbeta}')
-
 ## Mean level spacing
 
 def meanlvl(E):
     L = len(E)
     return (1/(L-1))*np.sum(np.diff(E))
 
-## Operator of evolution
-
-quasiENorm = []
-
-m2      = 4
-N2      = 2**m
-K2      = 100
-
 def lvlspacing(m,K,nbeta):
     N = 2**m
     quasiENorm = np.zeros((2*N -1,nbeta))
-    Psi = np.diag(np.ones(2*N))
+    Psi = np.diag(np.ones(2*N)) # Contains all possible states (first component is a dirac, the second one a ket of 0, and vice versa)
     p = np.linspace(start=-N, stop=N, endpoint=False, num=2*N) # impulsions
     x = np.pi/N * p                                    # positions
 
@@ -58,11 +27,11 @@ def lvlspacing(m,K,nbeta):
         
         for v in range(2*N):
             
-            psi_final = sim.loop(x, p, Psi[:,v], K, 1, b)
+            psi_final = sim.loop(x, p, Psi[:,v], K, 1, b) # Depends on the simulation you're using, here it is for the orthogonal case
             operator[:,v] = psi_final
                     
         val = np.linalg.eigvals(operator)
-        phi_n = np.sort(np.angle(val) % (2*np.pi))
+        phi_n = np.sort(np.angle(val) % (2*np.pi)) # /!\ WARNING ADD [::2] IN THE SYMPLECTIC CASE BECAUSE OF THE KRAMER DEGENERENSCY
         D = np.diff(phi_n)
         avg = meanlvl(phi_n) 
     
@@ -73,23 +42,8 @@ def lvlspacing(m,K,nbeta):
             print(f'loop {i}') 
     
     return np.ravel(quasiENorm)
-    
 
-bigbase = lvlspacing(7,10,200)
-smallbase = lvlspacing(2,10000,200)
-
-fig = plt.figure(2)
-ax1 = fig.add_subplot(121)
-ax1.set_title(f'Cas Orthogonal \n K=10000, N=4', fontsize=20)
-
-ax2 = fig.add_subplot(122)
-ax2.set_title('Poisson \n K=20, N=128', fontsize=20)
-
-ax1.hist(smallbase, 200, density=True)
-ax2.hist(bigbase, 150, density=True)
-plt.show()
-
-## Level Spacing distribution
+## Level Spacing distribution (does not include the Unitary case)
 
 def Poisson(s):
     return np.exp(-s)
@@ -97,18 +51,5 @@ def Poisson(s):
 def Orthogonal(s):
     return (s*np.pi/2)*np.exp(-s**2 * np.pi/4)
 
-
-s = np.linspace(start=0, stop=2*np.pi, num=2000)
-
-ax2.plot(s, Poisson(s), label='Poisson')
-ax2.legend()
-ax1.plot(s,Orthogonal(s), label='CSE')
-ax1.legend()
-
-ax1.set_xlabel('s', fontsize=15)
-ax1.set_ylabel('P(s)', fontsize=15)
-
-ax2.set_xlabel('s', fontsize=15)
-ax2.set_ylabel('P(s)', fontsize=15)
-
-plt.show()
+def Symplectic(s):
+    return (s**4 * 2**18 / 3**6 / np.pi**3)*np.exp(-s**2 * 64 / 9 /np.pi)
